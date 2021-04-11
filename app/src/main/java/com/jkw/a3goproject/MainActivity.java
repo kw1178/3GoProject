c5ebed8a8389d284fe6868822d36d88808e8ba3e
JO <kw11778@naver.com>
2021-04-11 11:07:37 +0900

    2

--- app/src/main/java/com/jkw/a3goproject/MainActivity.java
+++ app/src/main/java/com/jkw/a3goproject/MainActivity.java
@@ -3,8 +3,10 @@ package com.jkw.a3goproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
